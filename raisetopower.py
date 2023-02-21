ae24e1dfa8b598ef56386ec37288822539e8584b
Edmund Debrah <edmundking05@gmail.com>
2023-02-21 22:00:00 +0530

    add error handling to raiseToPower.py

--- raisetopower.py
+++ raisetopower.py
@@ -1,6 +1,10 @@
 def raiseToPower(n,x):
-    if (x == 1):
-        return n;
+    if (n >= 1):
+        if (x == 0):
+            return 1
+        elif (x == 1):
+            return n
+        else:
+            return n * raiseToPower(n, x - 1)
     else:
-        return n * raiseToPower(n, x - 1);
-
+        print("n must be greater or equal to 1")
